648e1ea863d9e5782064352057105052d1544a96
Thiago <thiagofp707@gmail.com>
2020-11-09 22:24:36 -0300

    Modularização do mailer para grandes disparos

--- mailer.php
+++ mailer.php
@@ -8,40 +8,55 @@
     require './PHPMailer/PHPMailer.php';
     require './PHPMailer/SMTP.php';
 
+   
+    setRecipients($emails, $nome, $email_envio, $senha_envio, $email_remetente, $nome_remetente, $email_reply, $subject, $template_path);
+    
 
-    $mail = new PHPMailer;
-    $mail->CharSet = 'UTF-8';
-    $mail->isSMTP();
-    $mail->SMTPDebug = 0;
-    $mail->Host = 'smtp.gmail.com';
-    $mail->Port = 587;
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    function config($email_envio, $senha_envio, $email_remetente, $nome_remetente, $email_reply, $subject, $template_path){
+
+        $mail = new PHPMailer;
+        $mail->CharSet = 'UTF-8';
+        $mail->isSMTP();
+        $mail->SMTPDebug = 0;
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 587;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
 
 
-    $mail->SMTPAuth = true;
-    $mail->Username = $email_envio;
-    $mail->Password = $senha_envio;
+        $mail->SMTPAuth = true;
+        $mail->Username = $email_envio;
+        $mail->Password = $senha_envio;
 
-    $mail->setFrom($email_remetente,$nome_remetente);
-    $mail->addReplyTo($email_reply);
+        $mail->setFrom($email_remetente,$nome_remetente);
+        $mail->addReplyTo($email_reply);
+        $mail->Subject = $subject;
 
-    foreach ($emails as $email) {
-        $mail->addBCC($email, $nome);
-        //$mail->addAddress($email, $nome);
+        //$mail->addAttachment('assets/arq.pdf');
+
+        $mail->isHTML(true);
+        $mail->msgHTML(file_get_contents($template_path), __DIR__);
+        $mail->AltBody = 'Mensagem Semana de Informática';
+
+        return $mail;
     }
 
-    $mail->Subject = $subject;
+    function setRecipients($emails, $nome, $email_envio, $senha_envio, $email_remetente, $nome_remetente, $email_reply, $subject, $template_path){
+        for($i=0; $i< count($emails); $i+=30){
+            $mail = config($email_envio, $senha_envio, $email_remetente, $nome_remetente, $email_reply, $subject, $template_path);
+            $bloco = array_slice($emails, $i, 30);
+            foreach ($bloco as $email) {
+                $mail->addBCC($email, $nome);
+            }
+            sendMail($mail);
+        }
+    }
 
-    $mail->isHTML(true);
-    $mail->msgHTML(file_get_contents($template_path), __DIR__);
-    $mail->AltBody = 'Mensagem Semana de Informática';
-
-    //$mail->addAttachment('assets/arq.pdf');
-
-    if (!$mail->send()) {
-        //echo 'Mailer Error: '. $mail->ErrorInfo;
-    } else {
-        echo "Enviado!";
+    function sendMail($mail){
+        if (!$mail->send()) {
+            //echo 'Mailer Error: '. $mail->ErrorInfo;
+        } else {
+            echo "Enviado!";
+        }
     }
 
 ?>
